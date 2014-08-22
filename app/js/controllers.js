5c55ee9a91f16cae97c6df2e2896e5db2c7849f5
activist <lodott@gmail.com>
2014-08-22 14:41:53 +0200

    Starting with original angular seed

---
+++ app/js/controllers.js
@@ -0,0 +1,11 @@
+'use strict';
+
+/* Controllers */
+
+angular.module('coursyApp.controllers', [])
+  .controller('MyCtrl1', ['$scope', function($scope) {
+
+  }])
+  .controller('MyCtrl2', ['$scope', function($scope) {
+
+  }]);
